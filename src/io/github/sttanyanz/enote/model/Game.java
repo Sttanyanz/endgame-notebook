eba4eeb41d3693df6274db6094b260f8f7f3406b
Sttanyanz <winavesh@gmail.com>
2024-04-01 21:44:20 +0300

    feat: add class Game

---
+++ src/io/github/sttanyanz/enote/model/Game.java
@@ -0,0 +1,28 @@
+package io.github.sttanyanz.enote.model;
+
+public class Game {
+    private final Player whitePlayer;
+
+    private final Player blackPlayer;
+
+    private final Board board;
+
+
+    public Game(Player whitePlayer, Player blackPlayer, Board board) {
+        this.whitePlayer = whitePlayer;
+        this.blackPlayer = blackPlayer;
+        this.board = board;
+    }
+
+    public Player getWhitePlayer() {
+        return whitePlayer;
+    }
+
+    public Player getBlackPlayer() {
+        return blackPlayer;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+}
