534ab1003b46e6d034ed1e904889528647753839
Sttanyanz <winavesh@gmail.com>
2024-04-01 21:45:24 +0300

    test: test class Game

---
+++ tst/io/github/sttanyanz/enote/model/GameTest.java
@@ -0,0 +1,35 @@
+package io.github.sttanyanz.enote.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void getWhite() {
+        final Player inputWhitePlayer = new Player("Emily", Color.white);
+        final Player expectedWhitePlayer = inputWhitePlayer;
+        final Game game = new Game(inputWhitePlayer, null, null);
+        final Player actualWhitePlayer = game.getWhitePlayer();
+        assertEquals(expectedWhitePlayer, actualWhitePlayer);
+    }
+
+    @Test
+    void getBlack() {
+        final Player inputBlackPlayer = new Player("Sarah", Color.white);
+        final Player expectedBlackPlayer = inputBlackPlayer;
+        final Game game = new Game(null, inputBlackPlayer, null);
+        final Player actualBlackPlayer = game.getBlackPlayer();
+        assertEquals(expectedBlackPlayer, actualBlackPlayer);
+    }
+
+    @Test
+    void getBoard() {
+        final Board inputBoard = new Board();
+        final Board expectedBoard = inputBoard;
+        final Game game = new Game(null, null, inputBoard);
+        final Board actualBoard = game.getBoard();
+        assertEquals(expectedBoard, actualBoard);
+    }
+}
