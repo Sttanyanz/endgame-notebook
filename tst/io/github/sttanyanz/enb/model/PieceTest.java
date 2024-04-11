d07ad20bce982f415a22153675c5a6b65c3b463e
Sttanyanz <winavesh@gmail.com>
2024-04-11 21:55:10 +0300

    test: new tests for Piece

--- tst/io/github/sttanyanz/enb/model/PieceTest.java
+++ tst/io/github/sttanyanz/enb/model/PieceTest.java
@@ -7,11 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class PieceTest {
 
     @Test
-    void testgetColor() {
+    void testGetColor() {
         final Piece inputPiece = new Piece(PieceName.pawn, Color.white);
         final Color expectedColor = Color.white;
         final Color actualColor = inputPiece.getColor();
 
         assertEquals(expectedColor, actualColor);
+    }
+
+    @Test
+    void testGetName() {
+        final Piece inputPiece = new Piece(PieceName.knight, Color.black);
+        final PieceName expectedName = PieceName.knight;
+        final PieceName actualName = inputPiece.getName();
+
+        assertEquals(expectedName, actualName);
     }
 }
